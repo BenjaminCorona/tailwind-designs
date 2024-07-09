e44de23d78df39e1ddef9782d8c605bb2d593120
benja13lara <benja13lara@gmail.com>
2024-07-09 15:44:40 +0000

    creacion del dashboard

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
-/** @type {import('tailwindcss').Config} */
+// tailwind.config.js
 module.exports = {
-  content: ["./src/**/*.html"],
+  content: [
+    './src/**/*.{html,js}', // Ajusta esto según tu estructura de archivos
+    './index.html', // Incluye este archivo si está en la raíz de tu proyecto
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
